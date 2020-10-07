b64f234797024a4e96919b63ee48e7945c7d193c
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-10-07 16:01:32 -0500

    post method works

--- employees/request.py
+++ employees/request.py
@@ -1,20 +1,20 @@
 EMPLOYEES = [
     {
         "name": "Emily",
-        "employeeId": 1,
-        "animalId": 5,
+        "locationId": 1,
+        "animalId": 3,
         "id": 1
     },
     {
         "name": "Sam",
-        "employeeId": 2,
+        "locationId": 2,
         "animalId": 3,
         "id": 2
     },
     {
         "name": "Esther",
-        "employeeId": 1,
-        "animalId": "5",
+        "locationId": 1,
+        "animalId": 3,
         "id": 3
     }
 ]
@@ -38,3 +38,19 @@ def get_single_employee(id):
             requested_employee = employee
 
     return requested_employee
+
+def create_employee(employee):
+    # Get the id value of the last employee in the list
+    max_id = EMPLOYEES[-1]["id"]
+
+    # Add 1 to whatever that number is
+    new_id = max_id + 1
+
+    # Add an `id` property to the employee dictionary
+    employee["id"] = new_id
+
+    # Add the employee dictionary to the list
+    EMPLOYEES.append(employee)
+
+    # Return the dictionary with `id` property added
+    return employee
