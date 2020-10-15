ca7a4a9de3b907920763736d2cf5d8bdd1be988b
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-10-15 09:42:06 -0500

    location info avaliable through JOIN

--- kennel.sql
+++ kennel.sql
@@ -62,3 +62,17 @@ SELECT * FROM Location;
 SELECT * FROM Customer;
 SELECT * FROM Employee;
 SELECT * FROM Animal;
+
+
+SELECT
+    a.id,
+    a.name,
+    a.breed,
+    a.status,
+    a.location_id,
+    a.customer_id,
+    l.name location_name,
+    l.address location_address
+FROM Animal a
+JOIN Location l
+    ON l.id = a.location_id
